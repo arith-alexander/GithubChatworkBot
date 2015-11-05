1d52dbe3e47ad2927e1c466fde7f883d3fd315c9
arith-alexander <alexander.k@arith-metic.jp>
2015-11-05 15:55:26 +0900

    empty config

--- cgi-bin/config.py
+++ cgi-bin/config.py
@@ -3,35 +3,12 @@
 # Global configuration file for GithubChatworkBot.
 # See detailed documentation inside gcbot.py script.
 
-chatwork_token = '40339ab6a5c351a9b1714e05e52412c7'
+chatwork_token = ''
 
 logging = True
 
 chatwork_github_account_map = {
-    "828301": "arith-kawai",
-    "1384793": "arith-banzui",
-    "1305354": "arith-andoreo",
-    "1457903": "arith-fukui",
-    "1492017": "arith-r-shudo",
-    "1298432": "arith-shimomura",
-    "1339585": "arith-otsuka",
-    "1484650": "arith-t-ooishi",
-    "1072183": "arith-suzukiakira",
-    "1566949": "arith-y-ono",
-    "1550555": "arith-alexander",
-    # ベトナムチーム
-    "1376630": "ramitata",
-    "1376624": "thangnt0602",
-    "1374758": "hattnalvn",
-    "1276322": "khietnt",
-    "1276330": "TranHoa",
-    "667476": "namgaf",
 }
 
 repository_room_map = {
-    "cwprcbot": "40130311",
-    "arismile-frontend": "39867847",
-    "arismile-admin": "39867847",
-    "arismile-documents": "39867847",
-    "once.kedamono.unity": "33605145"
 }
